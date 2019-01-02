c40c1a978b6a0210de8982839989d50db2468211
Fiona Ferstl <fiona.ferstl@students.fhnw.ch>
2019-01-02 17:29:24 +0100

    3 Auktion-Pages

---
+++ source/DotNetBay.WebApp/Controllers/AuctionsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.Mvc;
+using DotNetBay.Core;
+using DotNetBay.Data.EF;
+using DotNetBay.Data.Entity;
+using DotNetBay.WebApp.Models;
+
+namespace DotNetBay.WebApp.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        private EFMainRepository mainRepository;
+        private IAuctionService service;
+
+        public AuctionsController()
+        {
+            this.mainRepository = new EFMainRepository();
+
+            this.service = new AuctionService(this.mainRepository, new SimpleMemberService(this.mainRepository));
+        }
+
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            return View(this.service.GetAll().ToList());
+        }
+
+        // GET: Auctions/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Auctions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auctions/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "StartPrice,Title,Description,StartDateTimeUtc,EndDateTimeUtc")]
+            NewAuctionViewModel viewAuction)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var members = new SimpleMemberService(this.mainRepository);
+
+                var auction = new Auction()
+                {
+                    Title = viewAuction.Title,
+                    Description = viewAuction.Description,
+                    StartPrice = viewAuction.StartPrice,
+                    StartDateTimeUtc = viewAuction.StartDateTimeUtc,
+                    EndDateTimeUtc = viewAuction.EndDateTimeUtc,
+                    Seller = members.GetCurrentMember(),
+                };
+
+                this.service.Save(auction);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
--- source/DotNetBay.WebApp/Global.asax.cs
+++ source/DotNetBay.WebApp/Global.asax.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.EF;
 
 namespace DotNetBay.WebApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IAuctionRunner AuctionRunner { get; private set; }
+
         protected void Application_Start()
         {
+            //MVC related startup
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //DotNetBay related startup
+            var mainRepository = new EFMainRepository();
+            mainRepository.SaveChanges();
+
+            AuctionRunner = new AuctionRunner(mainRepository);
+            AuctionRunner.Start();
         }
     }
 }
