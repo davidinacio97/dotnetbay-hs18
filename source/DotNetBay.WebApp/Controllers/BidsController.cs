075fe23af19bd16dc85611a734c319f09900c592
Fiona Ferstl <fiona.ferstl@students.fhnw.ch>
2019-01-03 22:04:28 +0100

    4 Bid Page

---
+++ source/DotNetBay.WebApp/Controllers/BidsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNetBay.Core;
+using DotNetBay.Data.EF;
+using DotNetBay.WebApp.Models;
+
+namespace DotNetBay.WebApp.Controllers
+{
+    public class BidsController : Controller
+    {
+        private EFMainRepository mainRepository;
+        private IAuctionService service;
+
+        public BidsController()
+        {
+            this.mainRepository = new EFMainRepository();
+
+            this.service = new AuctionService(this.mainRepository, new SimpleMemberService(this.mainRepository));
+        }
+
+        // GET: Bids/Create?AuctionId=
+        public ActionResult Create(int auctionId)
+        {
+            var auction = service.GetAll().FirstOrDefault(a => a.Id == auctionId);
+
+            if (auction == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var newBidViewModel = new NewBidViewModel
+            {
+                AuctionTitle = auction.Title,
+                AuctionDescription = auction.Description,
+                StartPrice = auction.StartPrice,
+                CurrentPrice = auction.CurrentPrice,
+                BidAmount = Math.Max(auction.StartPrice, auction.CurrentPrice) + 1
+            };
+
+            return View(newBidViewModel);
+        }
+
+        // POST: Bids/Create
+        [HttpPost]
+        public ActionResult Create(NewBidViewModel newBid)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var auction = this.service.GetAll().FirstOrDefault(a => a.Id == newBid.AuctionId);
+
+                this.service.PlaceBid(auction, newBid.BidAmount);
+                return RedirectToAction("Index","Auctions");
+            }
+            return View();
+        }
+
+        // GET: Bids/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Bids/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bids/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Bids/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
