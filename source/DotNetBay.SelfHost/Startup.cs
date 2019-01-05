953d848be6c7d5bebd49f2bfb01b05c01ef4de75
davidinacio97 <davidinacio97@hotmail.ch>
2019-01-05 22:18:19 +0100

    Lab 09 - A1

--- source/DotNetBay.SelfHost/Startup.cs
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using DotNetBay.Health.Owin;
+using DotNetBay.WebApi;
 using Microsoft.Owin;
 using Owin;
 
@@ -12,9 +18,34 @@ namespace DotNetBay.SelfHost
     {
         public void Configuration(IAppBuilder app)
         {
+            // Lab 08 - OWIN
             app.UseHealth("/health");
+
+            // Lab 09 - Web API
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Services.Replace(typeof(IAssembliesResolver), new AssembliesResolver());
+            app.UseWebApi(config);
             
+
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+
+    class AssembliesResolver : DefaultAssembliesResolver
+    {
+        public override ICollection<Assembly> GetAssemblies()
+        {
+            ICollection<Assembly> assemblies = base.GetAssemblies();
+            var apiAssembly = Assembly.LoadFrom("DotNetBay.WebApi.dll");
+            assemblies.Add(apiAssembly);
+            return assemblies;
         }
     }
 }
---
+++ source/DotNetBay.WebApi/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DotNetBay.WebApi
+{
+    [RoutePrefix("status")]
+    public class StatusController : ApiController
+    {
+
+        [HttpGet]
+        public IHttpActionResult AreYouFine()
+        {
+            return this.Ok("I'm fine");
+        }
+    }
+}
