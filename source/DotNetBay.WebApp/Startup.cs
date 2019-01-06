5f8cb93201525133f9bdd4a5ecdb01610c3c8d12
davidinacio97 <davidinacio97@hotmail.ch>
2019-01-06 15:16:00 +0100

    A3.1 Api formatting settings

--- source/DotNetBay.WebApp/Startup.cs
+++ source/DotNetBay.WebApp/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using DotNetBay.WebApi.Controller;
 using DotNetBay.WebApp;
 using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -23,6 +26,13 @@ namespace DotNetBay.WebApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Configuration for Lab 09 A3.1
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             app.UseWebApi(config);
         }
     }
